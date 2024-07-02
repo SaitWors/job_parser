b8470df31d55ad8e0559c40185c60c0cba569481
Eterexx <144895838+malakall@users.noreply.github.com>
2024-07-02 08:57:28 +0300

    first commit

--- vacancies/management/commands/parse_vacancies.py
+++ vacancies/management/commands/parse_vacancies.py
@@ -54,3 +54,52 @@ class Command(BaseCommand):
 
 
 
+##
+
+class Command(BaseCommand):
+    help = 'Parse vacancies from HH.ru and save to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--filter', type=str, help='Filter vacancies by name or salary')
+        parser.add_argument('--value', type=str, help='Value for the filter')
+        parser.add_argument('--pages', type=int, default=1, help='Number of pages to parse')
+
+    def handle(self, *args, **options):
+        def get_vacancies(filter_type, filter_value, pages=1):
+            res = []
+            for page in range(pages):
+                params = {
+                    'text': f'{filter_value}',
+                    'page': page,
+                    'per_page': 100,
+                    'only_with_salary': 'true' if filter_type == 'salary' else 'false',
+                }
+                req = requests.get('https://api.hh.ru/vacancies/', params).json()
+                if 'items' in req.keys():
+                    res.extend(req['items'])
+                print('|', end='')
+            return res
+
+        # Получение параметров
+        filter_type = options['filter']
+        filter_value = options['value']
+        pages = options['pages']
+
+        # Сбор данных о вакансиях
+        vacancies_data = get_vacancies(filter_type, filter_value, pages)
+        total_vacancies = len(vacancies_data)  # Подсчет количества вакансий
+
+        # Сохранение данных в базу данных
+        for vac in vacancies_data:
+            Vacancy.objects.update_or_create(
+                hh_id=vac['id'],
+                defaults={
+                    'name': vac['name'],
+                    'salary_from': vac['salary']['from'] if vac['salary'] else None,
+                    'salary_to': vac['salary']['to'] if vac['salary'] else None,
+                    'working_days': vac['schedule']['name'] if 'schedule' in vac else '',
+                    'url': vac['alternate_url']
+                }
+            )
+
+        self.stdout.write(self.style.SUCCESS(f'Data collected and filtered. Total vacancies: {total_vacancies}'))
--- vacancies/views.py
+++ vacancies/views.py
@@ -73,4 +73,50 @@ class VacancyListView(ListView):
 
 
 
-   
+##
+from django.shortcuts import render
+from django.views import View
+from django.core.management import call_command
+from django.http import JsonResponse
+from .models import Vacancy
+
+class RunParseVacanciesCommandView(View):
+    def post(self, request, *args, **kwargs):
+        filter_type = request.POST.get('filter', '')
+        filter_value = request.POST.get('value', '')
+        pages = request.POST.get('pages', '1')
+
+        try:
+            pages = int(pages)  # Преобразование в целое число
+        except ValueError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid number of pages'}, status=400)
+
+        call_command('parse_vacancies', filter=filter_type, value=filter_value, pages=pages)
+        
+        total_vacancies = Vacancy.objects.count()
+        return JsonResponse({'status': 'success', 'total_vacancies': total_vacancies})
+
+    def get(self, request, *args, **kwargs):
+        filter_type = request.GET.get('filter', '')
+        filter_value = request.GET.get('value', '')
+        vacancies = Vacancy.objects.all()
+
+        if filter_type and filter_value:
+            if filter_type == 'name':
+                vacancies = vacancies.filter(name__icontains=filter_value)
+            elif filter_type == 'salary':
+                try:
+                    filter_value = int(filter_value)
+                    vacancies = vacancies.filter(salary_from__gte=filter_value)
+                except ValueError:
+                    pass
+
+        total_vacancies = vacancies.count()
+
+        context = {
+            'vacancies': vacancies,
+            'filter_type': filter_type,
+            'value': filter_value,
+            'total_vacancies': total_vacancies,
+        }
+        return render(request, 'vacancies/vacancy_list.html', context)
